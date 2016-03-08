9dc99b905c8e4a3ef9967d6093c6d75202f882e0
Prelang Builder <builder@prelang.com>
2016-03-08 18:05:35 +0000

    Generated model Customer with migration

---
+++ spec/factories/customers.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :customer do
+    first_name "MyString"
+    last_name "MyString"
+    company "MyString"
+    customer_type "MyString"
+    email "MyString"
+    phone "MyString"
+  end
+end
