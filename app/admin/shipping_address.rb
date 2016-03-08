894bcd5f1b90eb020628b5d7e6744d138a19b298
Prelang Builder <builder@prelang.com>
2016-03-08 18:06:50 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'ShippingAddress' to allow for create, update

--- app/admin/shipping_address.rb
+++ app/admin/shipping_address.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ShippingAddress do
+  permit_params :first_name, :last_name, :address_1, :address_2, :city, :state, :country, :zip, :email, :phone
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
