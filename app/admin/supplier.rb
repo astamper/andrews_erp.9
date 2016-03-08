e121ddb051748d4621331a86ee64ea533b93b433
Prelang Builder <builder@prelang.com>
2016-03-08 18:07:11 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Supplier' to allow for create, update

--- app/admin/supplier.rb
+++ app/admin/supplier.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Supplier do
+  permit_params :company_name
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
