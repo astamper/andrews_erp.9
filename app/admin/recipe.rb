e7f5a7c472780d07cb5e3820e19654840695f1f9
Prelang Builder <builder@prelang.com>
2016-03-08 18:06:18 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Recipe' to allow for create, update

--- app/admin/recipe.rb
+++ app/admin/recipe.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Recipe do
+  permit_params :quantity
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
