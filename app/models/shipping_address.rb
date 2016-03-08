02b15f3b2fba3e275aba3a5aa87cb217158094ce
Prelang Builder <builder@prelang.com>
2016-03-08 18:06:05 +0000

    Defined validation for attribute 'phone' on model 'ShippingAddress'

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,4 +3,5 @@ class ShippingAddress < ActiveRecord::Base
   belongs_to :order
   validates_formatting_of :zip, using: :us_zip
   validates_formatting_of :email, using: :email
+  validates_formatting_of :phone, using: :us_phone
 end
