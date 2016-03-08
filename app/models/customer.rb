64d0e5f54dca9925b7ec19ed322e22216923311a
Prelang Builder <builder@prelang.com>
2016-03-08 18:06:05 +0000

    Defined validation for attribute 'phone' on model 'Customer'

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,5 @@ class Customer < ActiveRecord::Base
   has_many :orders
   has_many :shipping_addresses
   validates_formatting_of :email, using: :email
+  validates_formatting_of :phone, using: :us_phone
 end
